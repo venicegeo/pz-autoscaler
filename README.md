d8f5354ee2392141526a25aed34421050b32a9e1
Patrick D <patrick.doody@rgi-corp.com>
2017-01-23 09:11:46 -0500

    Initializing CF Beans

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Piazza Autoscaler.
+
+This application depends on the following variables:
+- cf.username
+- cf.password
+- cf.apiHost
+- cf.organization
--- src/main/java/autoscaler/Application.java
+++ src/main/java/autoscaler/Application.java
@@ -15,8 +15,21 @@
  **/
 package autoscaler;
 
+import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.doppler.DopplerClient;
+import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
+import org.cloudfoundry.reactor.ConnectionContext;
+import org.cloudfoundry.reactor.DefaultConnectionContext;
+import org.cloudfoundry.reactor.TokenProvider;
+import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
+import org.cloudfoundry.reactor.doppler.ReactorDopplerClient;
+import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
+import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
+import org.cloudfoundry.uaa.UaaClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -28,4 +41,35 @@ public class Application {
 		SpringApplication.run(Application.class, args); // NOSONAR
 	}
 
+	@Bean
+	DefaultConnectionContext connectionContext(@Value("${cf.apiHost}") String apiHost) {
+		return DefaultConnectionContext.builder().apiHost(apiHost).build();
+	}
+
+	@Bean
+	PasswordGrantTokenProvider tokenProvider(@Value("${cf.username}") String username, @Value("${cf.password}") String password) {
+		return PasswordGrantTokenProvider.builder().password(password).username(username).build();
+	}
+
+	@Bean
+	ReactorCloudFoundryClient cloudFoundryClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
+		return ReactorCloudFoundryClient.builder().connectionContext(connectionContext).tokenProvider(tokenProvider).build();
+	}
+
+	@Bean
+	ReactorDopplerClient dopplerClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
+		return ReactorDopplerClient.builder().connectionContext(connectionContext).tokenProvider(tokenProvider).build();
+	}
+
+	@Bean
+	ReactorUaaClient uaaClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
+		return ReactorUaaClient.builder().connectionContext(connectionContext).tokenProvider(tokenProvider).build();
+	}
+
+	@Bean
+	DefaultCloudFoundryOperations cloudFoundryOperations(CloudFoundryClient cloudFoundryClient, DopplerClient dopplerClient,
+			UaaClient uaaClient, @Value("${cf.organization}") String organization, @Value("${SPACE}") String space) {
+		return DefaultCloudFoundryOperations.builder().cloudFoundryClient(cloudFoundryClient).dopplerClient(dopplerClient)
+				.uaaClient(uaaClient).organization(organization).space(space).build();
+	}
 }
